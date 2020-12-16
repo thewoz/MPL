e7acc2edd02fb2bf9293eb43278215af353dd313
Leonardo <leonardo.parisi@gmail.com>
2020-12-16 16:29:36 +0100

    update license

--- src/kabsh.cpp
+++ src/kabsh.cpp
@@ -1,10 +1,21 @@
-//
-//  main.cpp
-//  Kabsh
-//
-//  Created by Leonardo Parisi on 25/04/2020.
-//  Copyright © 2020 MPL. All rights reserved.
-//
+/*
+ * GNU GENERAL PUBLIC LICENSE
+ *
+ * Copyright (C) 2017
+ * Created by Leonardo Parisi (leonardo.parisi[at]gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <cstdlib>
 #include <cstdio>
