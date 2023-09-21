a127a8dbffde21d81f137bdff5c473284a00b8de
thewoz <leonardo.parisi@gmail.com>
2023-09-21 17:01:21 +0200

    Update kabsch.hpp

--- include/geometry/kabsch.hpp
+++ include/geometry/kabsch.hpp
@@ -78,17 +78,15 @@ namespace mpl::geometry::kabsch {
   //*****************************************************************************/
   namespace util_kabsch {
     
-    cv::Mat _solve(uint32_t size, uint32_t dims, const double * P, const double * Q, cv::Mat & R, cv::Mat & T, double * S = NULL, double * barycenter = NULL, double EPS = DBL_EPSILON) {
+    cv::Mat _solve(uint32_t size, uint32_t dims, const double * P, const double * Q, cv::Mat & R, cv::Mat & T, double * S = NULL, double * barycenterP = NULL, double EPS = DBL_EPSILON) {
             
       double * p0 = (double *) calloc(dims, sizeof(double));
       double * q0 = (double *) calloc(dims, sizeof(double));
       
-      if(barycenter != NULL && barycenter[0] != DBL_MAX) {
+      if(barycenterP != NULL && barycenterP[0] != DBL_MAX) {
         
-        for(uint32_t i=0; i<dims; ++i){
-          p0[i] = barycenter[i];
-          q0[i] = barycenter[i];
-        }
+        for(uint32_t i=0; i<dims; ++i)
+          p0[i] = barycenterP[i];
 
       } else {
         
@@ -100,6 +98,151 @@ namespace mpl::geometry::kabsch {
         }
         
         for(uint32_t i=0; i<dims; ++i){
+          p0[i] /= size;
+        }
+        
+      }
+      
+      for(uint32_t i=0; i<dims; ++i){
+        const double * row = &Q[i*size];
+        for(uint32_t k=0; k<size; ++k){
+          q0[i] += row[k];
+        }
+      }
+      
+      for(uint32_t i=0; i<dims; ++i){
+        q0[i] /= size;
+      }
+        
+      cv::Mat A = cv::Mat::zeros(dims, dims, CV_64F);
+      
+      for(uint32_t dp=0; dp<dims; ++dp){
+        
+        const double * rowP = &P[dp*size];
+        
+        for(uint32_t dq=0; dq<dims; ++dq){
+          
+          const double * rowQ = &Q[dq*size];
+          
+          for(uint32_t k=0; k<size; ++k){
+            
+            A.at<double>(dp,dq) += (rowP[k]-p0[dp]) * (rowQ[k]-q0[dq]);
+            
+          }
+          
+        }
+        
+      }
+      
+      cv::Mat W,U,V;
+      
+      cv::SVDecomp(A, W, U, V, cv::SVD::MODIFY_A | cv::SVD::FULL_UV);
+      
+      R = V.t() * U.t();
+      
+      if(cv::determinant(R) < 0){
+        V.col(dims-1) *= -1.0;
+        R = V.t() * U.t();
+      }
+      
+      T = cv::Mat(dims, 1, CV_64F);
+      
+      for(uint32_t i=0; i<dims; ++i){
+        T.at<double>(i) = q0[i] - p0[i];
+      }
+      
+      /* compute the optimal scaling */
+      if(S != NULL){
+        
+        if(size > 1){
+          
+          double nom = 0.0;
+          double dom = 0.0;
+          
+          double * RP = (double *) calloc(size, sizeof(double));
+          
+          for(uint32_t ir=0; ir<dims; ++ir) {
+            
+            memset(RP, 0, size*sizeof(double));
+            
+            double * rowR = R.ptr<double>(ir);
+            
+            for(uint32_t ip=0; ip<dims; ++ip){
+              
+              const double * rowP = &P[ip*size];
+              
+              for(uint32_t j=0; j<size; ++j){
+                RP[j] += rowR[ip] * (rowP[j]-p0[ip]);
+                
+              }
+              
+            }
+            
+            const double * rowQ = &Q[ir*size];
+            
+            for(uint32_t j=0; j<size; ++j){
+              nom += (rowQ[j]-q0[ir]) * RP[j];
+              dom += RP[j] * RP[j];
+            }
+            
+          }
+          
+          *S = nom / dom;
+          
+          free(RP);
+          
+        } else { *S = 1.0; }
+        
+      }
+  
+      // copio p0
+      cv::Mat ret;
+      ret.create(dims, 1, CV_64F);
+      
+      for(uint32_t i=0; i<dims; ++i){
+        ((double*)ret.data)[i] = p0[i];
+      }
+      
+      free(p0);
+      free(q0);
+      
+      return ret;
+      
+    }
+  
+    cv::Mat _solve(uint32_t size, uint32_t dims, const double * P, const double * Q, cv::Mat & R, cv::Mat & T, double * S = NULL, double * barycenterP = NULL, double * barycenterQ = NULL, double EPS = DBL_EPSILON) {
+            
+      double * p0 = (double *) calloc(dims, sizeof(double));
+      double * q0 = (double *) calloc(dims, sizeof(double));
+      
+      if(barycenterP != NULL && barycenterP[0] != DBL_MAX) {
+        
+        for(uint32_t i=0; i<dims; ++i)
+          p0[i] = barycenterP[i];
+
+      } else {
+        
+        for(uint32_t i=0; i<dims; ++i){
+          const double * row = &P[i*size];
+          for(uint32_t k=0; k<size; ++k){
+            p0[i] += row[k];
+          }
+        }
+        
+        for(uint32_t i=0; i<dims; ++i){
+          p0[i] /= size;
+        }
+        
+      }
+      
+      if(barycenterQ != NULL && barycenterQ[0] != DBL_MAX) {
+        
+        for(uint32_t i=0; i<dims; ++i)
+          q0[i] = barycenterQ[i];
+
+      } else {
+        
+        for(uint32_t i=0; i<dims; ++i){
           const double * row = &Q[i*size];
           for(uint32_t k=0; k<size; ++k){
             q0[i] += row[k];
@@ -107,7 +250,6 @@ namespace mpl::geometry::kabsch {
         }
         
         for(uint32_t i=0; i<dims; ++i){
-          p0[i] /= size;
           q0[i] /= size;
         }
         
@@ -193,8 +335,7 @@ namespace mpl::geometry::kabsch {
         } else { *S = 1.0; }
         
       }
-  
-      
+
       // copio p0
       cv::Mat ret;
       ret.create(dims, 1, CV_64F);
@@ -209,14 +350,13 @@ namespace mpl::geometry::kabsch {
       return ret;
       
     }
-  
     
   }
   
   //*****************************************************************************/
   // solve
   //*****************************************************************************/
-  cv::Mat solve(const cv::Mat & P, const cv::Mat & Q, cv::Mat & R, cv::Mat & T, double * S = NULL, double * barycenter = NULL, double EPS = DBL_EPSILON) {
+  cv::Mat solve(const cv::Mat & P, const cv::Mat & Q, cv::Mat & R, cv::Mat & T, double * S = NULL, double * barycenterP = NULL, double EPS = DBL_EPSILON) {
     
     assert(P.type()  == CV_64FC1);
     assert(Q.type()  == CV_64FC1);
@@ -225,7 +365,23 @@ namespace mpl::geometry::kabsch {
     assert(P.type()  == Q.type());
     assert(P.depth() == Q.depth());
         
-    return util_kabsch::_solve(P.cols, P.rows, (double*)P.data, (double*)Q.data, R, T, S, barycenter, EPS);
+    return util_kabsch::_solve(P.cols, P.rows, (double*)P.data, (double*)Q.data, R, T, S, barycenterP, EPS);
+    
+  }
+
+  //*****************************************************************************/
+  // solve
+  //*****************************************************************************/
+  cv::Mat solve(const cv::Mat & P, const cv::Mat & Q, cv::Mat & R, cv::Mat & T, double * S = NULL, double * barycenterP = NULL, double * barycenterQ = NULL, double EPS = DBL_EPSILON) {
+    
+    assert(P.type()  == CV_64FC1);
+    assert(Q.type()  == CV_64FC1);
+
+    assert(P.size()  == Q.size());
+    assert(P.type()  == Q.type());
+    assert(P.depth() == Q.depth());
+        
+    return util_kabsch::_solve(P.cols, P.rows, (double*)P.data, (double*)Q.data, R, T, S, barycenterP, barycenterQ, EPS);
     
   }
   
@@ -234,27 +390,55 @@ namespace mpl::geometry::kabsch {
   //*****************************************************************************/
   cv::Point3d solve3D(const cv::Mat & P, const cv::Mat & Q, cv::Mat & R, cv::Mat & T, double * S = NULL, cv::Point3d _barycenter = cv::Point3d(DBL_MAX,DBL_MAX,DBL_MAX), double EPS = DBL_EPSILON) {
 
-    double barycenter[3] = { _barycenter.x, _barycenter.y, _barycenter.z };
+    double barycenterP[3] = { _barycenterP.x, _barycenterP.y, _barycenterP.z };
 
     cv::Mat _p0 = solve(P, Q, R, T, S, barycenter, EPS);
 
     return cv::Point3d(((double*)_p0.data)[0],((double*)_p0.data)[1], ((double*)_p0.data)[2]);
 
   }
-  
+
+  //*****************************************************************************/
+  // solve3D
+  //*****************************************************************************/
+  cv::Point3d solve3D(const cv::Mat & P, const cv::Mat & Q, cv::Mat & R, cv::Mat & T, double * S = NULL, cv::Point3d _barycenterP = cv::Point3d(DBL_MAX,DBL_MAX,DBL_MAX),  cv::Point3d _barycenterQ = cv::Point3d(DBL_MAX,DBL_MAX,DBL_MAX), double EPS = DBL_EPSILON) {
+
+    double barycenterP[3] = { _barycenterP.x, _barycenterP.y, _barycenterP.z };
+    double barycenterQ[3] = { _barycenterQ.x, _barycenterQ.y, _barycenterQ.z };
+
+    cv::Mat _p0 = solve(P, Q, R, T, S, barycenterP, barycenterQ, EPS);
+
+    return cv::Point3d(((double*)_p0.data)[0],((double*)_p0.data)[1], ((double*)_p0.data)[2]);
+
+  }
+
   //*****************************************************************************/
   // solve2D
   //*****************************************************************************/
-  cv::Point2d solve2D(const cv::Mat & P, const cv::Mat & Q, cv::Mat & R, cv::Mat & T, double * S = NULL, cv::Point2d _barycenter = cv::Point2d(DBL_MAX,DBL_MAX), double EPS = DBL_EPSILON) {
+  cv::Point2d solve2D(const cv::Mat & P, const cv::Mat & Q, cv::Mat & R, cv::Mat & T, double * S = NULL, cv::Point2d _barycenterP = cv::Point2d(DBL_MAX,DBL_MAX), double EPS = DBL_EPSILON) {
         
-    double barycenter[2] = { _barycenter.x, _barycenter.y };
+    double barycenterP[2] = { _barycenterP.x, _barycenterP.y };
 
-    cv::Mat _p0 = solve(P, Q, R, T, S, barycenter, EPS);
+    cv::Mat _p0 = solve(P, Q, R, T, S, barycenterP, EPS);
 
     return cv::Point2d(((double*)_p0.data)[0],((double*)_p0.data)[1]);
     
   }
-  
+
+  //*****************************************************************************/
+  // solve2D
+  //*****************************************************************************/
+  cv::Point2d solve2D(const cv::Mat & P, const cv::Mat & Q, cv::Mat & R, cv::Mat & T, double * S = NULL, cv::Point2d _barycenterP = cv::Point2d(DBL_MAX,DBL_MAX), cv::Point2d _barycenterQ = cv::Point2d(DBL_MAX,DBL_MAX), double EPS = DBL_EPSILON) {
+        
+    double barycenterP[2] = { _barycenterP.x, _barycenterP.y };
+    double barycenterQ[2] = { _barycenterQ.x, _barycenterQ.y };
+
+    cv::Mat _p0 = solve(P, Q, R, T, S, barycenterP, barycenterQ, EPS);
+
+    return cv::Point2d(((double*)_p0.data)[0],((double*)_p0.data)[1]);
+    
+  }
+    
   //*****************************************************************************/
   // apply
   //*****************************************************************************/
